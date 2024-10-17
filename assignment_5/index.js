906aeec056b5b95ad7ae539c585749bed89ecc89
absadykov05 <absadykov4@gmail.com>
2024-10-17 23:50:35 +0500

    task 5

---
+++ assignment_5/index.js
@@ -0,0 +1,7 @@
+const table = document.getElementById("age-table");
+const labels = table.getElementsByTagName("label");
+const td = table.querySelector("td");
+const form = document.getElementsByTagName("form");
+const firstInput = form.querySelector("input");
+const inputs = form.querySelectorAll("input");
+const secondInput = inputs[1];
